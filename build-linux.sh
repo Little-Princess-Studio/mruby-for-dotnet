a28ae36fcecebbe66037cc8cceecf19541a1a468
Huisama <yuwenhuisama@outlook.com>
2024-11-02 13:11:34 +0800

    [chore] updatel build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# 检查是否传入了 clean 参数
-if [ "$1" == "clean" ]; then
+if [ "$1" = "clean" ]; then
     echo "Cleaning mruby and mruby-wrapper build cache"
     cd mruby
     rake clean
@@ -16,7 +15,7 @@ fi
 echo "Building mruby and mruby-wrapper for Windows"
 
 echo "Building mruby"
-./build-mruby-win.sh
+sh build-mruby-linux.sh
 
 echo "Building mruby-shared"
 cd mruby-shared
